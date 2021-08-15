c3a691f077f4fa17cd03befedcf53d1c42654c72
Jason Liao <jason.liao.2005@gmail.com>
2021-08-15 12:53:09 +0800

    Add project files.

---
+++ src/core/Travel.Domain/Enums/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel.Domain.Enums
+{
+    public enum Currency
+    {
+        PHP,
+        USD,
+        JPY,
+        EUR,
+        NOK
+    }
+}
