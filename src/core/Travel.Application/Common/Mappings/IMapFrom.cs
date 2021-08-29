a460ecf6dc9bb75bc5dbc40e4ff4bbb15c86d084
Jason Liao <jason.liao.2005@gmail.com>
2021-08-29 18:47:17 +0800

    edit:2021/8/29

---
+++ src/core/Travel.Application/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Travel.Application.Common.Mappings
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
---
+++ src/core/Travel.Application/TourLists/Queries/ExportTours/TourPackageFileRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travel.Application.Common.Mappings;
+
+namespace Travel.Application.TourLists.Queries.ExportTours
+{
+    public class TourPackageRecord:IMapFrom<TourPackage>
+    {
+        public string Name { get; set; }
+        public string MapLocation { get; set; }
+    }
+}
