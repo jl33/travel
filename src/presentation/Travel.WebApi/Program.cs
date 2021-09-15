60659a560258f056f05703e8bcb5b3096bac869d
Jason Liao <jason.liao.2005@gmail.com>
2021-09-15 15:31:19 +0800

    edit: 2021/9/15, Ch8 finish

--- src/presentation/Travel.WebApi/Program.cs
+++ src/presentation/Travel.WebApi/Program.cs
@@ -6,18 +6,51 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Travel.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var name = Assembly.GetExecutingAssembly().GetName();
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Aessembly", $"{name.Name}")
+                .Enrich.WithProperty("Assembly", $"{name.Version}")
+                .WriteTo.SQLite(Environment.CurrentDirectory + @"/Logs/log.db", restrictedToMinimumLevel: LogEventLevel.Information, storeTimestampInUtc: true)
+                .WriteTo.File(new CompactJsonFormatter(), Environment.CurrentDirectory + @"/Logs/log.json", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Information)
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
