ee1709b99ab23bba642e7c57fc1ec2782da82557
Jason Liao <jason.liao.2005@gmail.com>
2021-09-18 21:18:27 +0800

    edit:2021/9/18

---
+++ src/core/Travel.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travel.Application.Dtos.User;
+using Travel.Domain.Entities;
+
+namespace Travel.Application.Common.Interfaces
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        User GetById(int id);
+    }
+}
---
+++ src/core/Travel.Application/Dtos/User/AuthenticateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Travel.Application.Dtos.User
+{
+    public class AuthenticateRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
