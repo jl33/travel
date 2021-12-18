628a6b8afd43d6944420b6d0b7193e918d183b54
Jason Liao <jason.liao.2005@gmail.com>
2021-12-18 15:07:34 +0800

    practice: Ch17 finish

---
+++ src/presentation/vue-app/src/validators/index.js
@@ -0,0 +1,21 @@
+import { required,
+    email,
+    minLength,
+maxLength } from "vuelidate/lib/validators";
+
+export default {
+    login: {
+        email: {required,email},
+        password: {required,minLength: minLength(8)},
+    },
+    city:{
+        required,
+        maxLength: maxLength(90),
+    },
+    country:{
+        required,
+    },
+    about:{
+        required,
+    },
+};
